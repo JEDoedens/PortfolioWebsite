c5220864379acc7a329f7d7688c6aff84a2036b7
JD <doedensje11@gmail.com>
2019-02-28 21:32:41 +0100

    nog meer JPG's verandert

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ console.log("hoi")
 var image = document.getElementById("gezinfoto");
 
 image.addEventListener("mouseenter", function( event ) {
-   image.src = "images/Gezin1.JPG"
+   image.src = "images/Gezin1.jpg"
 });
 
 image.addEventListener("mouseleave", function( event ) {
-   image.src = "images/Gezin.JPG"
+   image.src = "images/Gezin.jpg"
 });
